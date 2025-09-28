13ca007811d1ec2dd83415eb87e00813fbe7b701
sarpstar <56429320+sarpstar@users.noreply.github.com>
2025-09-28 18:20:48 +0300

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,17 @@
+def meters_to_km(m):
+    return m / 1000
+
+def km_to_meters(km):
+    return km * 1000
+
+def kg_to_lb(kg):
+    return kg * 2.20462
+
+def lb_to_kg(lb):
+    return lb / 2.20462
+
+def c_to_f(c):
+    return (c * 9/5) + 32
+
+def f_to_c(f):
+    return (f - 32) * 5/9
